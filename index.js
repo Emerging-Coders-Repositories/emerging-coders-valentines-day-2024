b662f0503af05c71981de19373c6fcad1e182355
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-08 00:08:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const noButtonPhrases = [
+    "No", 
+    "Wait I think you meant to press yes?", 
+    "Are you sure?", 
+    "Really really really sure?", 
+    "Pookie wookie please", 
+    "Don't do to this", 
+    "You going to make me cry...", 
+    "You're breaking my heart :(((",
+];
+
+const numberOfTimesClicked = 0; 
+const yesButton = document.getElementById("yes-button"); 
+const noButton = document.getElementById("no-button");
